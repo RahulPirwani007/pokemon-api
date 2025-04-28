57f60a14b807bdb593c0b7e1e055df55ae6166cb
RahulPirwani007 <rahulpirwani2000@gmail.com>
2025-04-28 20:24:21 +0530

    main

--- src/Header.jsx
+++ src/Header.jsx
@@ -19,22 +19,21 @@ const Header = ({ onSearch }) => {
   };
 
   return (
-    <div className="h-[6rem] w-full flex justify-between items-center">
+    <div className="h-[6rem] w-full flex justify-between items-center py-2">
       <img
         src="https://www.gameskinny.com/wp-content/uploads/gameskinnyc/a/6/0/a6078c-2fd9033b2be04c9b947d1aac6cdaef60-a2551.gif"
         alt=""
-        className="h-[4rem] ml-4 cursor-pointer"
+        className="h-[3rem] sm:h-[4rem] ml-4 cursor-pointer"
       />
-      <div className="flex justify-center items-center relative">
+      <div className="flex justify-center items-center relative w-full sm:w-auto">
         <input
           type="search"
           placeholder="pokemon search"
-          className="outline-0 w-[30rem] border-2 border-blue-600 rounded-2xl pl-4 pr-20 py-2 pb-2.5"
+          className="outline-0 w-full sm:w-[30rem] border-2 border-blue-600 rounded-2xl pl-4 pr-20 py-2 pb-2.5"
           value={searchTerm}
           onChange={handleSearchChange}
           onKeyPress={handleKeyPress}
         />
-
         <button
           className="absolute right-2 top-1/2 -translate-y-1/2 bg-[#feca03] px-4 py-0.5 rounded-full text-blue-700 cursor-pointer"
           onClick={handleSearchClick}
